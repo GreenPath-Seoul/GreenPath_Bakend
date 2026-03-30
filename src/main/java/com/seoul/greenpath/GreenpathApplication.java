6e4fb9978d89772876724fd71f95babe4420f666
ckddlf <53569322+ckddlf@users.noreply.github.com>
2026-03-30 20:21:39 +0900

    스프링부트 초기 세팅

---
+++ src/main/java/com/seoul/greenpath/GreenpathApplication.java
@@ -0,0 +1,13 @@
+package com.seoul.greenpath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenpathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreenpathApplication.class, args);
+	}
+
+}
