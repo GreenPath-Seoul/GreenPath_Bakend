845529196315a938079ed0cdcd018ed392394233
ckddlf <53569322+ckddlf@users.noreply.github.com>
2026-04-04 20:22:27 +0900

    feat: yml 임시 수정

--- src/main/java/com/seoul/greenpath/global/config/DataInitializer.java
+++ src/main/java/com/seoul/greenpath/global/config/DataInitializer.java
@@ -4,6 +4,7 @@ import com.seoul.greenpath.domain.member.entity.Badge;
 import com.seoul.greenpath.domain.member.repository.BadgeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.List;
  */
 @Component
 @RequiredArgsConstructor
+@Order(1) // CsvDataInitializer 이전에 실행 (순서 명시)
 public class DataInitializer implements CommandLineRunner {
 
     private final BadgeRepository badgeRepository;
